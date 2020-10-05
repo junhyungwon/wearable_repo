f78f075fd1e32228f1926a12dd52068b4d8fd160
rupy <rupy@linkflow.co.kr>
2020-10-05 19:07:27 +0900

    1. insert gps enable/disable 2. fixed streaming channel and jpeg channel for nexx360.

--- sdk/sw_app/app/app_version.h
+++ sdk/sw_app/app/app_version.h
@@ -12,19 +12,29 @@
 /*----------------------------------------------------------------------------
  Defines referenced	header files
 -----------------------------------------------------------------------------*/
-
+#include "board_config.h"
 /*----------------------------------------------------------------------------
  Definitions and macro
 -----------------------------------------------------------------------------*/
 // USING LTE(LTE + WIFI) => USE_LTE = 1 & USE_WIRELESS = 1
 // USING WIFI(WIFI ONLY) => USE_LTE = 0 & USE_WIRELESS = 1 
 
-#define EN_WIFI_CLIENT          1
-#define SW_RELEASE				1
+#define SW_RELEASE					1
+#define TCX_MODEL   				MODEL_NAME
 
-#define TCX_MODEL   			MODEL_NAME
-
-#define FITT360_SW_VER      	"0.90.06" 	//# alpha version
-#define FITT360_HW_VER			"Rev.0.3"
+#if defined(NEXX360)
+#if SW_RELEASE
+#if SYS_CONFIG_NET
+#	define FITT360_SW_VER      		"2.06.00N"
+#else
+#	define FITT360_SW_VER      		"2.06.00B"
+#endif /* #if SYS_CONFIG_NET */
+#else
+#	define FITT360_SW_VER			"2.06.00.D"
+#endif	/* #if SW_RELEASE */
+#elif defined(NEXXONE)
+	#define FITT360_SW_VER      	"0.90.06" 	//# alpha version
+#endif
+#define FITT360_HW_VER				"Rev.0.3"
 
 #endif	/* _APP_VERSION_H_ */
