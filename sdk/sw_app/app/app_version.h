bc9c5b09330ffa28401b8c6690b2ea0994be98c3
hyung won jun <jay@linkflow.co.kr>
2021-01-21 20:13:39 +0900

    version up 2.07.07 --> 2.07.08

--- sdk/sw_app/app/app_version.h
+++ sdk/sw_app/app/app_version.h
@@ -27,15 +27,15 @@
 
 #if defined(NEXX360B)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.07.07B"
+#	define FITT360_SW_VER      		"2.07.08B"
 #	else
-#	define FITT360_SW_VER      		"2.07.07.D"
+#	define FITT360_SW_VER      		"2.07.08.D"
 #	endif
 #elif defined(NEXX360W)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.07.07N"
+#	define FITT360_SW_VER      		"2.07.08N"
 #	else
-#	define FITT360_SW_VER      		"2.07.07.D"
+#	define FITT360_SW_VER      		"2.07.08.D"
 #	endif
 
 #elif defined(NEXXONE)
