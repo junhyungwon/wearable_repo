d4bcb45ee23d046e59aefd9dbec6d09f4b88b5b5
yONg <ivere27@gmail.com>
2021-03-12 23:42:42 +0900

    [rtmp] introduce RTMP publishing feature
    * add libuv & librtmp submodules
    * libuv build script in makefile
    * 'USE_RTMP' macro & librtmp build script

--- sdk/sw_app/app/app_cap.c
+++ sdk/sw_app/app/app_cap.c
@@ -36,6 +36,11 @@
 #include "app_web.h"
 #include "app_gps.h"
 
+#if USE_RTMP
+#include "srs_librtmp.h"
+#include <uv.h>
+#endif
+
 #define H264_DUMP  0
 #define JPEG_DUMP  0
 
@@ -68,6 +73,142 @@ static FILE *jfp = NULL;
 /*----------------------------------------------------------------------------
  Declares a function prototype
 -----------------------------------------------------------------------------*/
+
+#ifdef USE_RTMP
+
+// wowza. fixme : need a config file
+// $ ffplay rtmp://3.35.55.222:1935/live2/myStream/key
+static const char *rtmp_endpoint  = "rtmp://3.35.55.222:1935/live2/myStream/key";
+
+// libuv. fixme : separated file.
+pthread_t *thread;
+uv_loop_t *loop;
+uv_async_t *async_rtmp_connect;
+uv_timer_t *timer;
+
+// librtmp
+static srs_rtmp_t rtmp = NULL;
+static int rtmp_ready = 0;
+
+void timer_cb (uv_timer_t* timer, int status) {
+    fprintf(stderr, "* timer check rtmp status %d.\n", rtmp_ready);
+    if (rtmp_ready == 0) {
+        int r = uv_async_send(async_rtmp_connect);
+    }
+}
+
+void async_cb_rtmp_connect(uv_async_t* async) {
+    rtmp_ready = 0;
+
+    // close first
+    if (rtmp != NULL)
+        srs_rtmp_destroy(rtmp);
+
+    // librtmp
+    rtmp = srs_rtmp_create(rtmp_endpoint);
+    if (srs_rtmp_handshake(rtmp) != 0) {
+        srs_human_trace("simple handshake failed.");
+        return;
+    }
+    srs_human_trace("simple handshake success");
+
+    if (srs_rtmp_connect_app(rtmp) != 0) {
+        srs_human_trace("connect vhost/app failed.");
+        return;
+    }
+    srs_human_trace("connect vhost/app success");
+
+    if (srs_rtmp_publish_stream(rtmp) != 0) {
+        srs_human_trace("publish stream failed.");
+        return;
+    }
+    srs_human_trace("publish stream success");
+
+    rtmp_ready = 1;
+}
+
+
+void async_cb(uv_async_t* async) {
+    // fixme : imem will be in a short time.
+    stream_info_t *ifr = (stream_info_t*)async->data;
+
+    // fprintf(stderr, "async_cb %d\n", ifr->b_size);
+    static int isFirst = 0;
+    if (isFirst == 0 && ifr->is_key) {
+        isFirst = 1;
+    }
+
+    if (isFirst == 1 && rtmp_ready == 1) {
+        char* data = ifr->addr;
+        auto size = ifr->b_size;
+        int nb_start_code = 0;
+
+        u_int32_t fps = DEFAULT_FPS;
+        u_int32_t dts = 1000/fps;
+        u_int32_t pts = dts;
+
+        // u_int32_t timestamp = ifr->t_sec * 1000 + ifr->t_msec;
+        // dts = timestamp;
+        // pts = timestamp;
+
+        // send out the h264 packet over RTMP
+        int ret = srs_h264_write_raw_frames(rtmp, data, size, dts, pts);
+        if (ret != 0) {
+            if (srs_h264_is_dvbsp_error(ret)) {
+                srs_human_trace("ignore drop video error, code=%d", ret);
+            } else if (srs_h264_is_duplicated_sps_error(ret)) {
+                srs_human_trace("ignore duplicated sps, code=%d", ret);
+            } else if (srs_h264_is_duplicated_pps_error(ret)) {
+                srs_human_trace("ignore duplicated pps, code=%d", ret);
+            } else {
+                srs_human_trace("send h264 raw data failed. ret=%d", ret);
+
+                rtmp_ready = 0;
+            }
+        }
+
+        // 5bits, 7.3.1 NAL unit syntax, 
+        // H.264-AVC-ISO_IEC_14496-10.pdf, page 44.
+/*
+        u_int8_t nut = (char) data[nb_start_code] & 0x1f;
+        srs_human_trace("sent packet: type=%s, time=%d, size=%d, fps=%d, b[%d]=%#x(%s)",
+                        srs_human_flv_tag_type2string(SRS_RTMP_TYPE_VIDEO), dts, size, fps, nb_start_code,
+                        (char) data[nb_start_code],
+                        (nut == 7 ? "SPS" : (nut == 8 ? "PPS" : (nut == 5 ? "I" : (nut == 1 ? "P" : "Unknown")))));
+*/
+
+
+    }
+
+    uv_close((uv_handle_t*)async, NULL);
+}
+
+
+void uv_thread(void* context){
+    fprintf(stderr, "* libuv will start event loop\n");
+
+    int r;
+    loop = malloc(sizeof(uv_loop_t));
+    uv_loop_init(loop);
+
+    async_rtmp_connect = malloc(sizeof(uv_async_t));
+    r = uv_async_init(loop, async_rtmp_connect, async_cb_rtmp_connect);
+
+    // fixme : keep the loop alive. timer every 10s
+    timer = malloc(sizeof(uv_timer_t));
+    uv_timer_init(loop, timer);
+    uv_timer_start(timer, timer_cb, 1000* 10, 1000* 10);
+
+    //Start loop
+    uv_run(loop, UV_RUN_DEFAULT);
+
+    fprintf(stderr, "* libuv loop exited\n");
+    uv_loop_close(loop);
+
+    pthread_exit(NULL);
+}
+#endif	// USE_RTMP
+
 void video_status(void)
 {
     int i, temp, count, ret, vcount = 0;
@@ -221,7 +362,15 @@ static void proc_vid_cap(void)
                         }
                     }
 #endif
-					
+
+#ifdef USE_RTMP
+                    // fire async_cb
+                    uv_async_t *async = malloc(sizeof(uv_async_t));
+                    int r = uv_async_init(loop, async, async_cb);
+                    async->data = (void*)ifr;
+
+                    r = uv_async_send(async);
+#endif
                     app_rtsptx_write((void *)ifr->addr, ifr->offset, ifr->b_size,
                                         ifr->is_key?FTYPE_VID_I:FTYPE_VID_P, STYPE_VID_CH1, captime);
                 }
@@ -326,6 +475,12 @@ int vid_cap_start(void)
 	//# Register call back with encoder
 	callback.newDataAvailableCb = (VENC_CALLBACK_NEW_DATA)ipc_bits_fxn;
 	Venc_registerCallback(&callback, NULL);
+
+#ifdef USE_RTMP
+    // create libuv thread
+    thread = malloc(sizeof(pthread_t));
+    pthread_create(thread, NULL, uv_thread, NULL);
+#endif
 
 	//#--- create thread
 	if(thread_create(&icap->vObj, THR_vid_cap, APP_THREAD_PRI, NULL) < 0) {
@@ -530,8 +685,10 @@ int app_cap_start(void)
 	Venc_start();
 
 	cap_enc_late_init();
-    
+
+#ifndef USE_RTMP
 	ctrl_enc_multislice() ; 
+#endif
 
 	aprintf("done!\n");
 
