8f49e456551c0b91f17503d85ac84c6d6b45dc55
hyung won jun <jay@linkflow.co.kr>
2021-07-20 11:16:33 +0900

    version up 2.09.05 --> 2.09.06

--- sdk/sw_app/app/app_version.h
+++ sdk/sw_app/app/app_version.h
@@ -27,33 +27,33 @@
 
 #if defined(NEXX360B)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.09.05B"
+#	define FITT360_SW_VER      		"2.09.06B"
 #	else
-#	define FITT360_SW_VER      		"2.09.05.D"
+#	define FITT360_SW_VER      		"2.09.06.D"
 #	endif
 #elif defined(NEXX360W)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.09.05N"
+#	define FITT360_SW_VER      		"2.09.06N"
 #	else
-#	define FITT360_SW_VER      		"2.09.05.D"
+#	define FITT360_SW_VER      		"2.09.06.D"
 #	endif
 #elif defined(NEXX360V)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.09.05V"
+#	define FITT360_SW_VER      		"2.09.06V"
 #	else
-#	define FITT360_SW_VER      		"2.09.05.D"
+#	define FITT360_SW_VER      		"2.09.06.D"
 #	endif
 #elif defined(NEXX360H)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.09.05H"
+#	define FITT360_SW_VER      		"2.09.06H"
 #	else
-#	define FITT360_SW_VER      		"2.09.05.D"
+#	define FITT360_SW_VER      		"2.09.06.D"
 #	endif
 #elif defined(NEXXONE)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"1.01.05"
+#	define FITT360_SW_VER      		"1.01.06"
 #	else
-#	define FITT360_SW_VER      		"1.01.05.D"
+#	define FITT360_SW_VER      		"1.01.06.D"
 #	endif
 #endif
 #endif	/* _APP_VERSION_H_ */
