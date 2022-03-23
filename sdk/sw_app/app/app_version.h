9e1e0150a462697401ba158f33246444e15f33e9
hyung won jun <jay@linkflow.co.kr>
2022-03-23 18:31:14 +0900

    1. version up 2.09.36/1.01.36 --> 2.09.38/1.01.38

--- sdk/sw_app/app/app_version.h
+++ sdk/sw_app/app/app_version.h
@@ -23,61 +23,61 @@
 #define TCX_MODEL   				MODEL_NAME
 
 /* Fixed Hardware Version */
-#define FITT360_HW_VER				"Rev.0.3" 
+#define FITT380_HW_VER				"Rev.0.3" 
 
-#if defined(NEXX360B)
+#if defined(NEXX380B)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.09.36B"
+#	define FITT380_SW_VER      		"2.09.38B"
 #	else
-#	define FITT360_SW_VER      		"2.09.36.D"
+#	define FITT380_SW_VER      		"2.09.38.D"
 #	endif
-#elif defined(NEXX360C)
+#elif defined(NEXX380C)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.09.36C"
+#	define FITT380_SW_VER      		"2.09.38C"
 #	else
-#	define FITT360_SW_VER      		"2.09.36.D"
+#	define FITT380_SW_VER      		"2.09.38.D"
 #	endif
-#elif defined(NEXX360W)
+#elif defined(NEXX380W)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.09.36N"
+#	define FITT380_SW_VER      		"2.09.38N"
 #	else
-#	define FITT360_SW_VER      		"2.09.36.D"
+#	define FITT380_SW_VER      		"2.09.38.D"
 #	endif
-#elif defined(NEXX360W_MUX)
+#elif defined(NEXX380W_MUX)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.09.36N_MUX"
+#	define FITT380_SW_VER      		"2.09.38N_MUX"
 #	else
-#	define FITT360_SW_VER      		"2.09.36.D"
+#	define FITT380_SW_VER      		"2.09.38.D"
 #	endif
 #elif defined(NEXXB)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.09.36"
+#	define FITT380_SW_VER      		"2.09.38"
 #	else
-#	define FITT360_SW_VER      		"2.09.36.D"
+#	define FITT380_SW_VER      		"2.09.38.D"
 #	endif
 #elif defined(NEXXB_ONE)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.09.36"
+#	define FITT380_SW_VER      		"2.09.38"
 #	else
-#	define FITT360_SW_VER      		"2.09.36.D"
+#	define FITT380_SW_VER      		"2.09.38.D"
 #	endif
-#elif defined(NEXX360H)
+#elif defined(NEXX380H)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.09.36H"
+#	define FITT380_SW_VER      		"2.09.38H"
 #	else
-#	define FITT360_SW_VER      		"2.09.36D"
+#	define FITT380_SW_VER      		"2.09.38D"
 #	endif
 #elif defined(NEXXONE)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"1.01.36"
+#	define FITT380_SW_VER      		"1.01.38"
 #	else
-#	define FITT360_SW_VER      		"1.01.36.D"
+#	define FITT380_SW_VER      		"1.01.38.D"
 #	endif
-#elif defined(NEXX360W_CCTV)
+#elif defined(NEXX380W_CCTV)
 #	if SW_RELEASE
-#	define FITT360_SW_VER      		"2.09.36N_CCTV"
+#	define FITT380_SW_VER      		"2.09.38N_CCTV"
 #	else
-#	define FITT360_SW_VER      		"2.09.36.D"
+#	define FITT380_SW_VER      		"2.09.38.D"
 #	endif
 #endif
 #endif	/* _APP_VERSION_H_ */
